08bd38998d7fc0d54634c59213fc544d028652bb
NAVOMITA GHOSHAL <64421634+NAV0MITA11GHOSHAL@users.noreply.github.com>
2020-09-24 13:19:57 +0530

    Create resource.php

---
+++ resource.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Computer Classroom</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+	<h2 align="center">LINKS TO ONLINE RESOURCES</h2>
+
+		<br>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR JAVA"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/java/index.htm">CLICK HERE TO LEARN JAVA</a>
+			<p><a href="https://www.youtube.com/watch?v=TBWX97e1E9g">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF JAVA</a></p>
+			<p><a href="https://docs.oracle.com/javase/specs/jls/se8/html/index.html">CLICK HERE TO READ THE BOOK ON JAVA</a></p>
+			</p>
+		</p>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR MYSQL"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/mysql/index.htm">CLICK HERE TO LEARN MYSQL</a>
+			<p><a href="https://www.youtube.com/watch?v=7S_tz1z_5bA">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF MYSQL</a></p>
+			<p><a href="https://books.goalkicker.com/MySQLBook/">CLICK HERE TO READ THE BOOK ON MYSQL</a></p>
+			</p>
+		</p>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR PYTHON"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/python/index.htm">CLICK HERE TO LEARN PYTHON</a>
+			<p><a href="https://www.youtube.com/watch?v=_uQrJ0TkZlc">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF PYTHON</a></p>
+			<p><a href="https://inventwithpython.com/cracking/">CLICK HERE TO READ THE BOOK ON PYTHON</a></p>
+			</p>
+		</p>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR PHP"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/php/index.htm">CLICK HERE TO LEARN PHP</a>
+			<p><a href="https://www.youtube.com/watch?v=OK_JCtrrv-c">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF PHP</a></p>
+			<p><a href="https://leanpub.com/php-pandas/read">CLICK HERE TO READ THE BOOK ON PHP</a></p>
+			</p>
+		</p>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR RUBY ON RAILS"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/ruby-on-rails/index.htm">CLICK HERE TO LEARN RUBY ON RAILS</a>
+			<p><a href="https://www.youtube.com/watch?v=GY7Ps8fqGdc">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF RUBY ON RAILS</a></p>
+			<p><a href="https://www.railstutorial.org/book">CLICK HERE TO READ THE BOOK ON RUBY ON RAILS</a></p>
+			</p>
+		</p>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR JAVASCRIPT"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/javascript/index.htm">CLICK HERE TO LEARN JAVASCRIPT</a>
+			<p><a href="https://www.youtube.com/watch?v=PkZNo7MFNFg">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF JAVASCRIPT</a></p>
+			<p><a href="https://eloquentjavascript.net/">CLICK HERE TO READ THE BOOK ON JAVASCRIPT</a></p>
+			</p>
+		</p>
+
+		<p align="center">
+			<?php echo "ONLINE RESOURCES FOR NODE.JS"?>
+			<p>
+			<a href="https://www.tutorialspoint.com/nodejs/index.htm">CLICK HERE TO LEARN NODE.JS</a>
+			<p><a href="https://www.youtube.com/watch?v=TlB_eWDSMt4">CLICK HERE TO WATCH THE VIDEO TUTORIAL OF NODE.JS</a></p>
+			<p><a href="https://books.goalkicker.com/NodeJSBook/">CLICK HERE TO READ THE BOOK ON NODE.JS</a></p>
+			</p>
+		</p>
+		
+</body>
+</html>
