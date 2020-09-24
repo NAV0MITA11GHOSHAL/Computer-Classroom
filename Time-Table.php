3f65199fef8813fdc277d3c94136c00d201a8d13
NAVOMITA GHOSHAL <64421634+NAV0MITA11GHOSHAL@users.noreply.github.com>
2020-09-24 13:19:20 +0530

    Create Time-Table.php

---
+++ Time-Table.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"content="width=device-width, initial-scale=1">
+  <title>Computer Classroom</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+	<h2 align="center">Computer Classroom Time-Table</h2>
+
+		<br>
+		<?php
+			echo "<table border = '1'>";
+			echo "<tr>";
+			echo "<th>DAY</th>";
+			echo "<th></th>";
+			echo "<th>9:00 - 10:00</th>";
+			echo "<th>10:00 - 11:00</th>";
+			echo "<th>11:00 - 12:00</th>";
+			echo "<th>12:00 - 13:00</th>";
+			echo "<th>13:00 - 14:00</th>";
+			echo "<th>14:00 - 15:00</th>";
+			echo "<th>15:00 - 16:00</th>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th rowspan = '2'>Monday</th>";
+			echo "<td>Subject</td>";
+			echo "<td>JAVA</td>";
+			echo "<td>PYTHON</td>";
+			echo "<td>RUBY ON RAILS</td>";
+			echo "<td>LUNCH</td>";
+			echo "<td>PHP</td>";
+			echo "<td>MYSQL</td>";
+			echo "<td>JAVASCRIPT</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<td>Teacher</td>";
+			echo "<td>ZEET DAS</td>";
+			echo "<td>RIYANKA CHAKRABORTY</td>";
+			echo "<td>BHASKAR RAMAKRISHNAN</td>";
+			echo "<td>TIME</td>";
+			echo "<td>UTSAV CHAKRABORTY</td>";
+			echo "<td>ARCHANA SHARMA</td>";
+			echo "<td>TARAK MEHTA</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th rowspan = '2'>Tuesday</th>";
+			echo "<td>Subject</td>";
+			echo "<td>JAVASCRIPT</td>";
+			echo "<td>RUBY ON RAILS</td>";
+			echo "<td>PYTHON</td>";
+			echo "<td>LUNCH</td>";
+			echo "<td>JAVA</td>";
+			echo "<td>PHP</td>";
+			echo "<td>MYSQL</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<td>Teacher</td>";
+			echo "<td>TARAK MEHTA</td>";
+			echo "<td>BHASKAR RAMAKRISHNAN</td>";
+			echo "<td>RIYANKA CHAKRABORTY</td>";
+			echo "<td>TIME</td>";
+			echo "<td>ZEET DAS</td>";
+			echo "<td>UTSAV CHAKRABORTY</td>";
+			echo "<td>ARCHANA SHARMA</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th rowspan = '2'>Wednesday</th>";
+			echo "<td>Subject</td>";
+			echo "<td>PYTHON</td>";
+			echo "<td>PHP</td>";
+			echo "<td>JAVASCRIPT</td>";
+			echo "<td>LUNCH</td>";
+			echo "<td>MYSQL</td>";
+			echo "<td>JAVA</td>";
+			echo "<td>RUBY ON RAILS</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<td>Teacher</td>";
+			echo "<td>RIYANKA CHAKRABORTY</td>";
+			echo "<td>UTSAV CHAKRABORTY</td>";
+			echo "<td>TARAK MEHTA</td>";
+			echo "<td>TIME</td>";
+			echo "<td>ARCHANA SHARMA</td>";
+			echo "<td>ZEET DAS</td>";
+			echo "<td>BHASKAR RAMAKRISHNAN</td>";
+			echo "<td></td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th rowspan = '2'>Thursday</th>";
+			echo "<td>Subject</td>";
+			echo "<td>JAVA</td>";
+			echo "<td>MYSQL</td>";
+			echo "<td>PHP</td>";
+			echo "<td>LUNCH</td>";
+			echo "<td>PYTHON</td>";
+			echo "<td>RUBY ON RAILS</td>";
+			echo "<td>JAVASCRIPT</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<td>Teacher</td>";
+			echo "<td>ZEET DAS</td>";
+			echo "<td>ARCHANA SHARMA</td>";
+			echo "<td>UTSAV CHAKRABORTY</td>";
+			echo "<td>TIME</td>";
+			echo "<td>RIYANKA CHAKRABORTY</td>";
+			echo "<td>BHASKAR RAMAKRISHNAN</td>";
+			echo "<td>TARAK MEHTA</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th rowspan = '2'>Friday</th>";
+			echo "<td>Subject</td>";
+			echo "<td>PHP</td>";
+			echo "<td>JAVASCRIPT</td>";
+			echo "<td>JAVA</td>";
+			echo "<td>LUNCH</td>";
+			echo "<td>RUBY ON RAILS</td>";
+			echo "<td>PYTHON</td>";
+			echo "<td>NODE.JS</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<td>Teacher</td>";
+			echo "<td>UTSAV CHAKRABORTY</td>";
+			echo "<td>TARAK MEHTA</td>";
+			echo "<td>ZEET DAS</td>";
+			echo "<td>TIME</td>";
+			echo "<td>BHASKAR RAMAKRISHNAN</td>";
+			echo "<td>RIYANKA CHAKRABORTY</td>";
+			echo "<td>ANKITA MAHATO</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<th rowspan = '2'>Saturday</th>";
+			echo "<td>Subject</td>";
+			echo "<td>RUBY ON RAILS</td>";
+			echo "<td>JAVA</td>";
+			echo "<td>MYSQL</td>";
+			echo "<td>LUNCH</td>";
+			echo "<td>JAVASCRIPT</td>";
+			echo "<td>NODE.JS</td>";
+			echo "<td>PHP</td>";
+			echo "</tr>";
+			echo "<tr>";
+			echo "<td>Teacher</td>";
+			echo "<td>BHASKAR RAMAKRISHNAN</td>";
+			echo "<td>ZEET DAS</td>";
+			echo "<td>ARCHANA SHARMA</td>";
+			echo "<td>TIME</td>";
+			echo "<td>TARAK MEHTA</td>";
+			echo "<td>ANKITA MAHATO</td>";
+			echo "<td>UTSAV CHAKRABORTY</td>";
+			echo "</tr>";
+		?>
+
+</body>  
+</html>
