cf1e9a98a4cf4af0c7e2a912a73fc93a5ff7fbbd
NAVOMITA GHOSHAL <64421634+NAV0MITA11GHOSHAL@users.noreply.github.com>
2020-10-01 10:48:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,6 @@
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-  
-  <?php
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-$bberry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
-$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
-$webos = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
-if ($android || $bberry || $iphone || $ipod || $webos== true) 
-{ 
-header('Location: https://computer-classroom.herokuapp.com/');
-}
-?>
 
   <h1 class="logo" align="center">COMPUTER CLASSROOM</h1>
         
