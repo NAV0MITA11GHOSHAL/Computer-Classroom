b50c4cc5bdccc96ff45a39f715dfa10387b38e8b
NAVOMITA GHOSHAL <64421634+NAV0MITA11GHOSHAL@users.noreply.github.com>
2020-11-01 16:33:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
   <title>Computer Classroom</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <link rel="stylesheet" href="responsive.css">
 </head>
